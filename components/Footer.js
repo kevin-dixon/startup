9a9b57920f1908391112b85f6968a4beeb85be5a
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2024-11-05 10:36:00 -0700

    simon react

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,21 @@
 function Footer() {
-    return <h2>Hi, I am a Footer!</h2>;
+    return <footer class="background-tertiary text-center fixed-bottom opacity-85 text-white">
+
+        <nav class="row p-2 pb-0">
+            <a class="col-5 light-link" href="/pages/index.html">Index Page</a>
+            <div class="col-2">
+                <img class="d-inline-block align-top" width="50" height="50" src="/images/hat_logo.png"></img>
+            </div>
+            <div class="col-5">
+                <label>Kevin Dixon: </label>
+                <a class="light-link" href="https://github.com/kevin-dixon/startup">GitHub Repo</a>
+            </div>
+        </nav>
+        <div class="text-center p-2 pt-0">
+            <label>&#8482 2024 Trademark:</label>
+            <a class="light-link" href="https://mafiamystery.click/">MafiaMystery.click</a>
+        </div>
+    </footer>;
   }
   
   export default Footer;
