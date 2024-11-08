e0aa4b03e33fb96600fcc7a972b3497f7e1a7e9b
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2024-11-08 12:21:26 -0700

    react vite compiling

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center">Component!!!</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
