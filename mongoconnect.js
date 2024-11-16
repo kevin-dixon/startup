8dcef3062dfc949af9f8eb15a7a41cb0312eeaea
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2024-11-16 11:50:11 -0700

    implementing mongodb connect

---
+++ mongoconnect.js
@@ -0,0 +1,33 @@
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://<username>:<password>@/sample_airbnb?retryWrites=true&w=majority";
+    const myUri = "mongodb+srv://writekevind:1T5:16rejoicemon@mafiaclustera.vlpjq.mongodb.net/?retryWrites=true&w=majority&appName=MafiaClusterA";
+
+
+    const client = new MongoClient(myUri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
