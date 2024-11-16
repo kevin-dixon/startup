be73e337c7d1db570e33998633b603c8db4f9de9
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2024-11-16 12:08:35 -0700

    credential management working

--- mongoconnect.js
+++ mongoconnect.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}/?retryWrites=true&w=majority&appName=MafiaClusterA`;
+const pass = encodeURIComponent(config.password)
+const url = `mongodb+srv://${config.userName}:${pass}@${config.hostname}/?retryWrites=true&w=majority&appName=MafiaClusterA`;
 
 async function main(){
 
